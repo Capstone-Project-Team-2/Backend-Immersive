2e37eee3d8bc276e4ec8bf2cb2b88dbc5f943ff8
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-10-01 22:37:16 +0700

    crud buyer: add interface

--- features/buyers/data/query.go
+++ features/buyers/data/query.go
@@ -1,1 +1,47 @@
 package data
+
+import (
+	"capstone-tickets/features/buyers"
+
+	"gorm.io/gorm"
+)
+
+type buyerQuery struct {
+	db *gorm.DB
+}
+
+// Delete implements buyers.BuyerDataInterface.
+func (*buyerQuery) Delete(id string) error {
+	panic("unimplemented")
+}
+
+// Insert implements buyers.BuyerDataInterface.
+func (*buyerQuery) Insert(input buyers.BuyerCore) error {
+	panic("unimplemented")
+}
+
+// Login implements buyers.BuyerDataInterface.
+func (*buyerQuery) Login(email string, password string) (buyers.BuyerCore, error) {
+	panic("unimplemented")
+}
+
+// Select implements buyers.BuyerDataInterface.
+func (*buyerQuery) Select(id string) (buyers.BuyerCore, error) {
+	panic("unimplemented")
+}
+
+// SelectAll implements buyers.BuyerDataInterface.
+func (*buyerQuery) SelectAll() ([]buyers.BuyerCore, error) {
+	panic("unimplemented")
+}
+
+// Update implements buyers.BuyerDataInterface.
+func (*buyerQuery) Update(input buyers.BuyerCore) error {
+	panic("unimplemented")
+}
+
+func New(database *gorm.DB) buyers.BuyerDataInterface {
+	return &buyerQuery{
+		db: database,
+	}
+}
--- features/buyers/entities.go
+++ features/buyers/entities.go
@@ -1,5 +1,7 @@
 package buyers
 
+import "github.com/labstack/echo/v4"
+
 type BuyerCore struct {
 	ID          uint
 	Name        string
@@ -7,4 +9,31 @@ type BuyerCore struct {
 	Email       string
 	Password    string
 	Address     string
+}
+
+type BuyerDataInterface interface {
+	Login(email, password string) (BuyerCore, error)
+	SelectAll() ([]BuyerCore, error)
+	Select(id string) (BuyerCore, error)
+	Insert(input BuyerCore) error
+	Update(input BuyerCore) error
+	Delete(id string) error
+}
+
+type BuyerServiceInterface interface {
+	Login(email, password string) (BuyerCore, string, error)
+	Create(input BuyerCore) error
+	GetAll() ([]BuyerCore, error)
+	GetById(id string) (BuyerCore, error)
+	UpdateById(input BuyerCore) error
+	DeleteById(id string) error
+}
+
+type BuyerHandlerInterface interface {
+	Login(c echo.Context) error
+	Create(c echo.Context) error
+	GetAll(c echo.Context) error
+	GetById(c echo.Context) error
+	UpdateById(c echo.Context) error
+	DeleteById(c echo.Context) error
 }
