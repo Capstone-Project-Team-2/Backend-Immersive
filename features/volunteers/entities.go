1907b84d3a086eb5fedee744c86d3aba943f20f9
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-10-03 19:27:07 +0700

    crud: volunteer

--- features/volunteers/entities.go
+++ features/volunteers/entities.go
@@ -3,7 +3,25 @@ package volunteers
 type VolunteerCore struct {
 	ID       string
 	EventID  string
-	Name     string
-	Email    string
-	Password string
+	Name     string `validate:"required"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required"`
+}
+
+type VolunteerDataInterface interface {
+	Login(email, password string) (VolunteerCore, string, error)
+	Insert(input VolunteerCore) error
+	SelectAll() ([]VolunteerCore, error)
+	Select(id string) (VolunteerCore, error)
+	Update(id string, input VolunteerCore) error
+	Delete(id string) error
+}
+
+type VolunteerServiceInterface interface {
+	Login(email, password string) (VolunteerCore, string, error)
+	Create(input VolunteerCore) error
+	GetAll() ([]VolunteerCore, error)
+	GetById(id string) (VolunteerCore, error)
+	UpdateById(id string, input VolunteerCore) error
+	DeleteById(id string) error
 }
--- features/volunteers/handler/handler.go
+++ features/volunteers/handler/handler.go
@@ -1,1 +1,129 @@
 package handler
+
+import (
+	"capstone-tickets/features/volunteers"
+	"capstone-tickets/helpers"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+var log = helpers.Log()
+
+type VolunteerHandler struct {
+	volunteerService volunteers.VolunteerServiceInterface
+}
+
+func New(service volunteers.VolunteerServiceInterface) *VolunteerHandler {
+	return &VolunteerHandler{
+		volunteerService: service,
+	}
+}
+
+func (h *VolunteerHandler) Login(c echo.Context) error {
+	var login LoginRequest
+	err := c.Bind(&login)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, helpers.WebResponse(http.StatusBadRequest, helpers.Error400, nil))
+	}
+
+	id, token, err := h.volunteerService.Login(login.Email, login.Password)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helpers.WebResponse(http.StatusInternalServerError, err.Error(), nil))
+	}
+	var data = map[string]any{
+		"id":    id,
+		"token": token,
+	}
+	return c.JSON(http.StatusOK, helpers.WebResponse(http.StatusOK, "Login success", data))
+}
+
+func (h *VolunteerHandler) Create(c echo.Context) error {
+	var volunteerReq VolunteerRequest
+
+	errBind := c.Bind(&volunteerReq)
+	if errBind != nil {
+		log.Error("handler - error on bind request")
+		fmt.Println(errBind)
+		return c.JSON(http.StatusBadRequest, helpers.WebResponse(http.StatusBadRequest, helpers.Error400, nil))
+	}
+
+	newInput := VolunteerRequestToCore(volunteerReq)
+
+	err := h.volunteerService.Create(newInput)
+	if err != nil {
+		log.Error("handler-internal server error")
+		return c.JSON(http.StatusInternalServerError, helpers.WebResponse(http.StatusInternalServerError, helpers.Error500, nil))
+	}
+	return c.JSON(http.StatusCreated, helpers.WebResponse(http.StatusCreated, "operation success", nil))
+
+}
+func (h *VolunteerHandler) GetAll(c echo.Context) error {
+	//_, role := middlewares.ExtractToken(c)
+	//if role != "Admin" {
+	//return c.JSON(http.StatusUnauthorized, helpers.WebResponse(http.StatusUnauthorized, helpers.Error401, nil))
+	//}
+	result, err := h.volunteerService.GetAll()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helpers.WebResponse(http.StatusInternalServerError, helpers.Error500, nil))
+	}
+	var partnerResp = ListVolunteerCoreToResponse(result)
+	return c.JSON(http.StatusOK, helpers.FindAllWebResponse(http.StatusOK, "operation success", partnerResp, false))
+}
+
+func (h *VolunteerHandler) GetById(c echo.Context) error {
+	//id, _ := middlewares.ExtractToken(c)
+	idParam := c.Param("volunteer_id")
+	//if id != idParam {
+	//	return c.JSON(http.StatusUnauthorized, helpers.WebResponse(http.StatusUnauthorized, helpers.Error401, nil))
+	//}
+	result, err := h.volunteerService.GetById(idParam)
+	if err != nil {
+		if strings.Contains(err.Error(), "no row affected") {
+			return c.JSON(http.StatusBadRequest, helpers.WebResponse(http.StatusBadRequest, helpers.Error400, nil))
+		}
+		return c.JSON(http.StatusInternalServerError, helpers.WebResponse(http.StatusInternalServerError, helpers.Error500, nil))
+	}
+	var volunteerResponse = VolunteerCoreToResponse(result)
+	return c.JSON(http.StatusOK, helpers.WebResponse(http.StatusOK, "operation success", volunteerResponse))
+}
+
+func (h *VolunteerHandler) UpdateById(c echo.Context) error {
+	//id, _ := middlewares.ExtractToken(c)
+	idParam := c.Param("volunteer_id")
+	//if id != idParam {
+	//	return c.JSON(http.StatusUnauthorized, helpers.WebResponse(http.StatusUnauthorized, helpers.Error401, nil))
+	//}
+
+	var volunteerReq VolunteerRequest
+	errBind := c.Bind(&volunteerReq)
+	if errBind != nil {
+		log.Error("handler - error on bind request")
+		fmt.Println(errBind)
+		return c.JSON(http.StatusBadRequest, helpers.WebResponse(http.StatusBadRequest, helpers.Error400, nil))
+	}
+
+	updatedData := VolunteerRequestToCore(volunteerReq)
+
+	err := h.volunteerService.UpdateById(idParam, updatedData)
+	if err != nil {
+		log.Error("handler-internal server error")
+		return c.JSON(http.StatusInternalServerError, helpers.WebResponse(http.StatusInternalServerError, helpers.Error500, nil))
+	}
+	return c.JSON(http.StatusOK, helpers.WebResponse(http.StatusOK, "operation success", nil))
+}
+
+func (h *VolunteerHandler) DeleteById(c echo.Context) error {
+	//id, _ := middlewares.ExtractToken(c)
+	idParam := c.Param("volunteer_id")
+	//if id != idParam {
+	//	return c.JSON(http.StatusUnauthorized, helpers.WebResponse(http.StatusUnauthorized, helpers.Error401, nil))
+	//}
+	err := h.volunteerService.DeleteById(idParam)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helpers.WebResponse(http.StatusInternalServerError, helpers.Error500, nil))
+	}
+	return c.JSON(http.StatusOK, helpers.WebResponse(http.StatusOK, "operation success", nil))
+}
