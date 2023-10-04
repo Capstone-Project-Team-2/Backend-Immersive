db7e3b42ab19b434d7745323428ea62b628c01a7
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-10-04 14:34:34 +0700

    creat & get: transaction

--- features/transactions/data/query.go
+++ features/transactions/data/query.go
@@ -1,1 +1,54 @@
 package data
+
+import (
+	"capstone-tickets/features/transactions"
+	"capstone-tickets/helpers"
+
+	"gorm.io/gorm"
+)
+
+var log = helpers.Log()
+
+type transactionQuery struct {
+	db *gorm.DB
+}
+
+func New(database *gorm.DB) transactions.TransactionDataInterface {
+	return &transactionQuery{
+		db: database,
+	}
+}
+
+// Insert implements transactions.TransactionDataInterface.
+func (r *transactionQuery) Insert(data transactions.TransactionCore) (transactions.TransactionCore, error) {
+	transactionData := TransactionCoreToModel(data)
+	var err error
+	transactionData.ID, err = helpers.GenerateUUID()
+	if err != nil {
+		return transactions.TransactionCore{}, err
+	}
+
+	tx := r.db.Create(&transactionData)
+	if tx.Error != nil {
+		return transactions.TransactionCore{}, tx.Error
+	}
+	dataResp := TransactionModelToCore(transactionData)
+	return dataResp, nil
+}
+
+// Select implements transactions.TransactionDataInterface.
+func (r *transactionQuery) Select(id string) (transactions.TransactionCore, error) {
+	var transaction Transaction
+
+	tx := r.db.Where("id=?", id).First(&transaction)
+	if tx.Error != nil {
+		return transactions.TransactionCore{}, tx.Error
+	}
+	data := TransactionModelToCore(transaction)
+	return data, nil
+}
+
+// Update implements transactions.TransactionDataInterface.
+func (*transactionQuery) Update(id string, updatedData transactions.TransactionCore) error {
+	panic("unimplemented")
+}
--- features/transactions/entities.go
+++ features/transactions/entities.go
@@ -4,20 +4,22 @@ import "time"
 
 type TransactionCore struct {
 	ID            string
+	OrderID       string
 	BuyerID       string
+	EventID       string
 	PaymentStatus string
 	PaymentMethod string
 	TimeLimit     time.Time
 	TicketCount   uint
-	PaymentTotal  uint
+	PaymentTotal  float64
 }
 type TransactionDataInterface interface {
-	Insert(transactionData TransactionCore) (string, error)
+	Insert(data TransactionCore) (TransactionCore, error)
 	Select(id string) (TransactionCore, error)
 	Update(id string, updatedData TransactionCore) error
 }
 type TransactionServiceInterface interface {
-	Create(transactionData TransactionCore) (string, error)
+	Create(data TransactionCore) (TransactionCore, error)
 	Get(id string) (TransactionCore, error)
 	Update(id string, updatedData TransactionCore) error
 }
