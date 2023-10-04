1334d948c92582c80f0ba5fce6a49b994f5a1870
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-10-04 10:21:25 +0700

    config: midtrans

--- apps/config/config.go
+++ apps/config/config.go
@@ -11,12 +11,14 @@ import (
 var JWT_SECRRET = ""
 
 type AppConfig struct {
-	DBUsername string
-	DBPassword string
-	DBHost     string
-	DBPort     int
-	DBName     string
-	jwtKey     string
+	DBUsername          string
+	DBPassword          string
+	DBHost              string
+	DBPort              int
+	DBName              string
+	jwtKey              string
+	KEY_SERVER_MIDTRANS string
+	KEY_CLIENT_MIDTRANS string
 }
 
 func InitConfig() *AppConfig {
@@ -52,6 +54,14 @@ func ReadENV() *AppConfig {
 		app.DBName = val
 		isRead = false
 	}
+	if val, found := os.LookupEnv("KEY_SERVER_MIDTRANS"); found {
+		app.KEY_SERVER_MIDTRANS = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("KEY_CLIENT_MIDTRANS"); found {
+		app.KEY_CLIENT_MIDTRANS = val
+		isRead = false
+	}
 	if isRead {
 		viper.AddConfigPath(".")
 		viper.SetConfigName("local")
@@ -69,6 +79,8 @@ func ReadENV() *AppConfig {
 		app.DBHost = viper.Get("DBHOST").(string)
 		app.DBPort, _ = strconv.Atoi(viper.Get("DBPORT").(string))
 		app.DBName = viper.Get("DBNAME").(string)
+		app.KEY_SERVER_MIDTRANS = viper.Get("KEY_SERVER_MIDTRANS").(string)
+		app.KEY_CLIENT_MIDTRANS = viper.Get("KEY_CLIENT_MIDTRANS").(string)
 	}
 	JWT_SECRRET = app.jwtKey
 	return &app
