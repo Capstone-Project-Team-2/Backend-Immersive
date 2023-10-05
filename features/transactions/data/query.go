7e1255df68472e85b4dfaee40a4d85b76b636245
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-10-05 11:36:39 +0700

    create: transaction

--- features/transactions/data/query.go
+++ features/transactions/data/query.go
@@ -6,6 +6,7 @@ import (
 	"capstone-tickets/helpers"
 	"errors"
 
+	"github.com/midtrans/midtrans-go"
 	"gorm.io/gorm"
 )
 
@@ -45,21 +46,61 @@ func (r *transactionQuery) Insert(data transactions.TransactionCore) (transactio
 
 	//4. hitung total pembayaran
 	ticketPrice := tickets.TicketCore.Price
-	data.PaymentTotal
+	//gimana cara membaca perjenis ticketnya
+	paymentTotal := float64(data.TicketCount) * ticketPrice
 
+	//5. Membuat transaksi
 	transactionData := TransactionCoreToModel(data)
 	var err error
 	transactionData.ID, err = helpers.GenerateUUID()
 	if err != nil {
 		return transactions.TransactionCore{}, err
 	}
+	transactionData.PaymentTotal = paymentTotal
 
-	tx := r.db.Create(&transactionData)
+	//6. simpan ke database
+	tx = r.db.Create(&transactionData)
 	if tx.Error != nil {
+		tx.Rollback()
 		return transactions.TransactionCore{}, tx.Error
 	}
+
+	//7. kirim ke midtrans
+	_, err = SendTransactionToMidtrans(transactionData)
+	if err != nil {
+		tx.Rollback()
+		return transactions.TransactionCore{}, err
+	}
+
+	tx.Commit()
 	dataResp := TransactionModelToCore(transactionData)
 	return dataResp, nil
+}
+
+// SendTransactionToMidtrans sends transaction data to Midtrans
+func SendTransactionToMidtrans(transaction Transaction.TransactionCore, paymentMethod string) (*midtrans.TransactionResponse, error) {
+	coreGateway := midtrans.CoreGateway{
+		ClientKey: midtrans.ClientKey,
+		ServerKey: midtrans.ServerKey,
+		Env:       midtrans.Sandbox, // or midtrans.Production for production environment
+	}
+
+	chargeReq := &midtrans.ChargeReq{
+		PaymentType: midtrans.PaymentType(paymentMethod),
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  transaction.OrderID,
+			GrossAmt: int64(transaction.PaymentTotal),
+		},
+		// ... konfigurasi lainnya sesuai dengan jenis pembayaran yang Anda gunakan
+	}
+
+	// Kirim data transaksi ke Midtrans
+	chargeResp, err := coreGateway.ChargeTransaction(chargeReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return chargeResp, nil
 }
 
 // Select implements transactions.TransactionDataInterface.
